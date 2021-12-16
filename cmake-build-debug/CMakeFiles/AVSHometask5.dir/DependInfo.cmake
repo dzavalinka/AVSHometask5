46d3d0dca11b5ab2af1adc28d532a83deb0c0168
dzavalinka <93940375+dzavalinka@users.noreply.github.com>
2021-12-16 23:23:09 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/AVSHometask5.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/MiNot/Desktop/AVSHometask5/Guests.cpp" "C:/Users/MiNot/Desktop/AVSHometask5/cmake-build-debug/CMakeFiles/AVSHometask5.dir/Guests.cpp.obj"
+  "C:/Users/MiNot/Desktop/AVSHometask5/Hotel.cpp" "C:/Users/MiNot/Desktop/AVSHometask5/cmake-build-debug/CMakeFiles/AVSHometask5.dir/Hotel.cpp.obj"
+  "C:/Users/MiNot/Desktop/AVSHometask5/Room.cpp" "C:/Users/MiNot/Desktop/AVSHometask5/cmake-build-debug/CMakeFiles/AVSHometask5.dir/Room.cpp.obj"
+  "C:/Users/MiNot/Desktop/AVSHometask5/main.cpp" "C:/Users/MiNot/Desktop/AVSHometask5/cmake-build-debug/CMakeFiles/AVSHometask5.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/AVSHometask5.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "AVSHometask5.exe"
+  "AVSHometask5.exe.manifest"
+  "AVSHometask5.lib"
+  "AVSHometask5.pdb"
+  "CMakeFiles/AVSHometask5.dir/Guests.cpp.obj"
+  "CMakeFiles/AVSHometask5.dir/Hotel.cpp.obj"
+  "CMakeFiles/AVSHometask5.dir/Room.cpp.obj"
+  "CMakeFiles/AVSHometask5.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AVSHometask5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
